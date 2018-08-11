c5e9c585df3756de379e2d7ba47ea32fc36e6a3a
P.surovenko <grdg2010@mail.ru>
2018-08-12 00:58:01 +0600

    Readme

--- src/main/java/org/globus/App.java
+++ src/main/java/org/globus/App.java
@@ -1,13 +1,23 @@
 package org.globus;
 
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 /**
  * Hello world!
- *
  */
 public class App {
-    public static void main( String[] args ) {
-    System.out.println( "Hello World!" );
-    System.out.println( "third commit" );
-    System.out.println( "fourth commit" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("third commit");
+        System.out.println("fourth commit");
+
+
+
     }
 }
