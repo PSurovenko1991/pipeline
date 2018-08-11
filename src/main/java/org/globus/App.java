2b6af9a22bbd09228d25b1d9619b7fb9bd4adc03
P.surovenko <grdg2010@mail.ru>
2018-08-11 17:40:11 +0600

    Add checkstyle

--- src/main/java/org/globus/App.java
+++ src/main/java/org/globus/App.java
@@ -4,12 +4,10 @@ package org.globus;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("third commit");
-        System.out.println("fourth commit");
+public class App {
+    public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+    System.out.println( "third commit" );
+    System.out.println( "fourth commit" );
     }
 }
